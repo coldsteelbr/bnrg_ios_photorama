a83884de9d09333fa50b78006f1f42142403a09e
Roman <cold.steel.br@gmail.com>
2018-06-02 16:44:33 +0700

    NavCtrl, ImageView, AppIcons

---
+++ bnrg_ios_photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  bnrg_ios_photorama
+//
+//  Created by Roman Brazhnikov on 02.06.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
